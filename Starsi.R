972fd488b6c1a109d3bb19a80a1055265e799295
MGruszczynska <56296819+MGruszczynska@users.noreply.github.com>
2020-05-22 21:40:42 +0200

    Update Starsi.R

--- Starsi.R
+++ Starsi.R
@@ -1,17 +1,18 @@
 library("dplyr")
 library("RColorBrewer")
-styczen20_JC<-read.csv("JC-202001-citibike-tripdata.csv")
-luty20_JC<-read.csv("JC-202002-citibike-tripdata.csv")
 marzec20_JC<-read.csv("JC-202003-citibike-tripdata.csv")
-styczen19_JC<-read.csv("JC-201901-citibike-tripdata.csv")
-luty19_JC<-read.csv("JC-201902-citibike-tripdata.csv")
 marzec19_JC<-read.csv("JC-201903-citibike-tripdata.csv")
-
+marzec18_JC<-read.csv("JC-201803-citibike-tripdata.csv")
 
 wiek1<-function(df1=marzec19_JC,df2=marzec20_JC,...){
   library("dplyr")
   marzec20_JC<-read.csv("JC-202003-citibike-tripdata.csv")
   marzec19_JC<-read.csv("JC-201903-citibike-tripdata.csv")
+  marzec18_JC<-read.csv("JC-201803-citibike-tripdata.csv")
+  m18<-arrange(marzec18_JC,desc(birth.year))
+  wszyscy_18<-nrow(m18)
+  starsi60_18<-nrow(filter(m18, birth.year<=1959))
+  procent_starsi_18<-(starsi60_18/wszyscy_18)*100
   m19<-arrange(marzec19_JC,desc(birth.year))
   wszyscy_19<-nrow(m19)
   starsi60_19<-nrow(filter(m19, birth.year<=1959))
@@ -21,7 +22,7 @@ wiek1<-function(df1=marzec19_JC,df2=marzec20_JC,...){
   starsi60_20<-nrow(filter(m20, birth.year<=1959))
   procent_starsi_20<-(starsi60_20/wszyscy_20)*100
   par(mfcol=c(1,3))
-  barplot(c(wszyscy_19,wszyscy_20), main="Liczba u¿ytkowników",names.arg = c("2019","2020"),ylab="Liczba u¿ytkowników",col=c("mediumaquamarine","plum3"))
-  barplot(c(starsi60_19,starsi60_20),main="Liczba u¿ytkowników powy¿ej 60 roku ¿ycia",names.arg = c("2019","2020"),ylab="Liczba u¿ytkowników",col=c("mediumaquamarine","plum3"))
-  barplot(c(procent_starsi_19,procent_starsi_20),main="Procentowy udzia³ u¿ytkowników \n powy¿ej 60 roku ¿ycia",names.arg = c("2019","2020"),ylab = "Procentowy udzia³",col=c("mediumaquamarine","plum3"))
+  barplot(c(wszyscy_18,wszyscy_19,wszyscy_20), main="Liczba uÅ¼ytkownikÃ³w",names.arg = c("2018","2019","2020"),ylab="Liczba uÅ¼ytkownikÃ³w",col=c("mediumaquamarine","plum3","tan2"))
+  barplot(c(starsi60_18,starsi60_19,starsi60_20),main="Liczba uÅ¼ytkownikÃ³w powyÅ¼ej 60 roku Å¼ycia",names.arg = c("2018","2019","2020"),ylab="Liczba uÅ¼ytkownikÃ³w",col=c("mediumaquamarine","plum3","tan2"))
+  barplot(c(procent_starsi_18,procent_starsi_19,procent_starsi_20),main="Procentowy udziaÅ‚ uÅ¼ytkownikÃ³w \n powyÅ¼ej 60 roku Å¼ycia",names.arg = c("2018","2019","2020"),ylab = "Procentowy udziaÅ‚",col=c("mediumaquamarine","plum3","tan2"))
 }
