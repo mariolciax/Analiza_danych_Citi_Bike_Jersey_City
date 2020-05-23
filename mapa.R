61ca96a6d39adf6384151d5b9b3a19cf6cc7031a
MGruszczynska <56296819+MGruszczynska@users.noreply.github.com>
2020-05-23 22:42:39 +0200

    Add files via upload

---
+++ mapa.R
@@ -0,0 +1,5 @@
+map3 <- Leaflet$new()
+map3$setView(c(40.715, -74.05), zoom = 13)
+map3$marker(c(40.71959, -74.04312), bindPopup = "<p> Grove St PATH </p>")
+map3$marker(c(	40.71687, -74.03281), bindPopup = "<p> cos </p>")
+map3
